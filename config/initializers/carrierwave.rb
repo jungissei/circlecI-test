551ae277e92b29c9c25db38dc43a6cbf300c18d1
jungissei <issei_matsuhashi@yahoo.co.jp>
2020-05-25 21:45:08 +0900

    43

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,13 @@
+
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
+    config.storage = :fog
+
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
@@ -7,7 +15,8 @@ unless Rails.env.development? || Rails.env.test?
       region: ENV['AWS_REGION']
     }
 
-    config.fog_directory  = 'nginx-rails-app'
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
+    config.asset_host = ENV['AWS_BUCKET_URL']
     config.cache_storage = :fog
   end
 end
